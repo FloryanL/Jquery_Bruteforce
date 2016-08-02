5e07a5564858252d9d42e78bfe79f2716b49c559
FloryanL <floryan.lollivier@gmail.com>
2016-08-02 09:44:03 +0200

    test pas résolu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 /*Jquery*/
+//console.log();
 
-console.log(hey);
+var mdp = {}
 
+$.ajax({
+    url: 'http://docusland.fr/code-academie/js_bruteforce/data/dico.txt', // La ressource ciblée
+    success: function(result){
+		mdp = result.split("\n");
+		testPassword(mdp[i]);
+}
+});
